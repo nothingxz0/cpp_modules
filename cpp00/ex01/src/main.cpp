2edfc72e4d65922cdc3b0d18334f738db98ce62a
Soufiane Lasfar <slasfar@c1r1p7.1337.ma>
2025-12-08 18:38:05 +0100

    small changes and EOF

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -1,11 +1,19 @@
 #include "PhoneBook.hpp"
 #include <iostream>
-
+#include <string>
 int main() {
     PhoneBook   phoneBook;
     std::string command;
+
+    std::cout << "* HELLO THIS IS PHONEBOOK IT CAN COUNTAIN UP TO 8 CONTACTS *" << std::endl;
+    std::cout << "OPTIONS:\nADD: add a contact\nSEARCH: search for a contact\nEXIT: exit..." << std::endl;
     while (true) {
+        std::cout << "ENTER A COMMAND: " << std::flush;
         std::getline(std::cin, command);
+        if (std::cin.eof()) {
+            std::cout << "\nEOF!" << std::endl;
+            return (1);
+        }
         if (command == "ADD")
             phoneBook.addContact();
         else if (command == "SEARCH")
--- cpp00/test.cpp
+++ cpp00/test.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
-#include <iomanip>
 
-int main(){
-    std::cout << std::setw(5) << "5";
+class Test {
+    private:
+        int x;
+        int y;
+        static int hell;
+    public:
+        Test() {
+            hell = 6;
+            x = 0;
+            y = 5;
+        }
+        int get_value() {
+            return (x);
+        }
+        void set_value (int value) {
+            x = value;
+        }
+        static int  hell_yeah(int value) {
+            hell = value;
+            return (hell);
+        }
+};
+int main (){
+    Test hel;
+    std::cout << hel.hell_yeah(20);
+    std::cout << Test::hell_yeah(10);
 }
