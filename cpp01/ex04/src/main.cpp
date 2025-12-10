737649cdf35c64f1b48b425dd33310f1e9e309f3
nothingxz0 <lasfars312@gmail.com>
2025-12-10 21:29:40 +0100

    fix problem

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -8,23 +8,22 @@ int main(int ac, char **av) {
         return (1);
     }
     std::string fileName(av[1]), s1(av[2]), s2(av[3]), line;
-    std::ifstream inFile(fileName);
+    std::ifstream inFile(fileName.c_str());
     if (!inFile.is_open()) {
         std::cout << "Could not open the file for reading" << std::endl;
         return (1);
     }
-    std::ofstream outFile(fileName + ".replace");
+    std::ofstream outFile((fileName + ".replace").c_str());
     if (!outFile.is_open()) {
         std::cout << "Could not open the file for writing" << std::endl;
         return (1);
     }
     size_t pos;
     while (std::getline(inFile, line)) {
-       pos = line.find(s1);
-       if (pos != std::string::npos) {
-            std::string before = line.substr(0, pos);
-            std::string after = line.substr(pos + s1.length());
-            line = before + s2 + after;
+       pos = 0;
+       while ((pos = line.find(s1, pos)) != std::string::npos) {
+            line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+            pos += s2.length();
        }
         outFile << line << std::endl;
     }
