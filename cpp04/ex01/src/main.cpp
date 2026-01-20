fa31128e2723fdcb5f60837153a04664cceb164d
nothingxz0 <lasfars312@gmail.com>
2026-01-20 14:51:08 +0100

    mcheena

--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -11,6 +11,21 @@ int main() {
     const Animal* i = new Cat();
     std::cout << j->getType() << std::endl;
     std::cout << i->getType() << std::endl;
+    Dog good;
+    Dog goodd;
+    Dog bad(good);
+    good.setIdea(0, "Chase the cat");
+    std::cout << "Good Dog Idea[0]: ";
+    good.getIdea(0);
+    std::cout << "Bad Dog Idea[0]: ";
+    bad.getIdea(0);
+    goodd = good;
+    std::cout << "Goodd Dog Idea[0]: ";
+    goodd.getIdea(0);
+    bad = goodd;
+    std::cout << "Bad Dog Idea[0] after assignement from Goodd: ";
+    bad.getIdea(0);
+
     delete j;
     delete i;
     return 0;
