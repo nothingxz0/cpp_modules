6bf3155b108a7707ff600acb709d2a29075ad1a8
Soufiane Lasfar <lasfars312@gmail.com>
2026-09-09 20:02:49 +0100

    hell

--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,5 +1,7 @@
 #include "MutantStack.hpp"
+#include <stack>
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -16,9 +18,27 @@ int main()
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
+
     while (it != ite) {
         std::cout << *it << std::endl;
         ++it;
     }
+    std::stack<int> s(mstack);
+    std::cout << "sliced stack size: " << s.size() << std::endl;
+
+    MutantStack<std::string> sstack;
+    sstack.push("hello");
+    sstack.push("world");
+    for (MutantStack<std::string>::iterator sit = sstack.begin(); sit != sstack.end(); ++sit)
+        std::cout << *sit << std::endl;
+
+    MutantStack<int> empty;
+    std::cout << "empty: " << empty.empty() << " size: " << empty.size() << std::endl;
+
+    MutantStack<int> copy(mstack);
+    MutantStack<int> assigned;
+    assigned = mstack;
+    std::cout << "copy size: " << copy.size() << " assigned size: " << assigned.size() << std::endl;
+
     return 0;
 }
